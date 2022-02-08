e13f7b1947ad2e6b882220004d19ba3c66579460
Bishal Khanal <bishal.khanal87@gmail.com>
2022-02-08 15:51:54 -0800

    add alerts and prompts

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 'use strict';
 
+console.log('Welcome!');
+
 let userName = prompt('What is your Name?');
-alert(`Hello ${userName}! Welcome! Please guess my Favories!`);
+alert(`Hello ${userName}! Welcome! Lets play a Guessing game!`);
 
 let answerOne = prompt('Is Inception my Favorite Movie?').toLowerCase();
 
@@ -10,6 +12,7 @@ if(answerOne === 'yes' || answerOne ==='y'){
 }else if(answerOne === 'no' || answerOne ==='n'){
   alert('You are incorrect! Inception is my all time Fav movie.');
 }
+console.log(`${userName} answered ${answerOne}`);
 
 let answerTwo = prompt('Do I enjoy coding?').toLowerCase();
 
@@ -18,13 +21,36 @@ if(answerTwo === 'yes' || answerTwo ==='y'){
 }else if(answerTwo === 'no' || answerTwo ==='n'){
   alert('You are incorrect! I love Coding.');
 }
+console.log(`${userName} answered ${answerTwo}`);
 
 let answerThree = prompt('Do I live in Washington State?').toLowerCase();
 
 if(answerThree === 'yes' || answerThree ==='y'){
   alert ('You guessed it right!');
 }else if(answerThree === 'no' || answerThree ==='n'){
-  alert('Wrong! I do in WA state.');
+  alert('Wrong! I do live in WA state.');
 }
+console.log(`${userName} answered ${answerThree}`);
+
+let answerFour = prompt('Is my favorite band Megadeth?').toUpperCase();
+
+if(answerFour === 'YES' || answerFour ==='Y'){
+  alert ('You are correct!');
+}else if(answerFour === 'NO' || answerFour ==='N'){
+  alert('You are wrong.Megadeth is my fav Band!');
+}
+console.log(`${userName} answered ${answerFour}`);
+
+let answerFive = prompt('Do I have a dog?').toUpperCase();
+
+if(answerFive === 'YES' || answerFive === 'Y'){
+  alert('You are right!');
+}else if(answerFive === 'NO' || answerFive ==='N');{
+  alert('Incorrect. I do have a dog.');
+}
+console.log(`${userName} answered ${answerFive}`);
+
+
+
 
 
