882f125bd9d5145f1e18304e73f10831614d3b31
Bishal Khanal <bishal.khanal87@gmail.com>
2022-02-09 16:51:15 -0800

    added while loops to guess the number game

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+let counter = 0;
 
 console.log('Welcome!');
 
@@ -7,50 +8,92 @@ alert(`Hello ${userName}! Welcome! Lets play a Guessing game! Please answer eith
 
 let answerOne = prompt('Is Inception my Favorite Movie?').toLowerCase();
 
-if(answerOne === 'yes' || answerOne ==='y'){
-  alert ('You are correct!');
-}else if(answerOne === 'no' || answerOne ==='n'){
+if (answerOne === 'yes' || answerOne === 'y') {
+  alert('You are correct!');
+  counter = counter+1;
+} else if (answerOne === 'no' || answerOne === 'n') {
   alert('You are incorrect! Inception is my all time Fav movie.');
 }
 console.log(`${userName} answered ${answerOne}`);
 
 let answerTwo = prompt('Do I enjoy coding?').toLowerCase();
 
-if(answerTwo === 'yes' || answerTwo ==='y'){
-  alert ('You are correct!');
-}else if(answerTwo === 'no' || answerTwo ==='n'){
+if (answerTwo === 'yes' || answerTwo === 'y') {
+  alert('You are correct!');
+  counter = counter+1;
+} else if (answerTwo === 'no' || answerTwo === 'n') {
   alert('You are incorrect! I love Coding.');
 }
 console.log(`${userName} answered ${answerTwo}`);
 
 let answerThree = prompt('Do I live in Washington State?').toLowerCase();
 
-if(answerThree === 'yes' || answerThree ==='y'){
-  alert ('You guessed it right!');
-}else if(answerThree === 'no' || answerThree ==='n'){
+if (answerThree === 'yes' || answerThree === 'y') {
+  alert('You guessed it right!');
+  counter = counter+1;
+} else if (answerThree === 'no' || answerThree === 'n') {
   alert('Wrong! I do live in WA state.');
 }
 console.log(`${userName} answered ${answerThree}`);
 
 let answerFour = prompt('Is my favorite band Megadeth?').toUpperCase();
 
-if(answerFour === 'YES' || answerFour ==='Y'){
-  alert ('You are correct!');
-}else if(answerFour === 'NO' || answerFour ==='N'){
+if (answerFour === 'YES' || answerFour === 'Y') {
+  alert('You are correct!');
+  counter=counter+1;
+} else if (answerFour === 'NO' || answerFour === 'N') {
   alert('You are wrong.Megadeth is my fav Band!');
 }
 console.log(`${userName} answered ${answerFour}`);
 
 let answerFive = prompt('Do I have a dog?').toUpperCase();
 
-if(answerFive === 'YES' || answerFive === 'Y'){
+if (answerFive === 'YES' || answerFive === 'Y') {
   alert('You are right!');
-}else if(answerFive === 'NO' || answerFive ==='N'){
+  counter=counter+1;
+} else if (answerFive === 'NO' || answerFive === 'N') {
   alert('Incorrect. I do have a dog.');
 }
 console.log(`${userName} answered ${answerFive}`);
 
-alert(`Thank you for playing the game ${userName}! That was fun!`);
+alert(`Thank you for playing the game ${userName}! Now lets play a game where you guess the number that I am thinking of!`);
+
+let myNumber = 5;
+let userNumber = prompt('What number am I thinking of?');
+let userTry = 3;
+
+while (userNumber != myNumber) {
+  userTry--;
+
+
+  if (userNumber > myNumber) {
+    alert('Too High! keep guessing');
+    userNumber = prompt('What number am i Thinking of?');
+
+
+  }
+  else if (userNumber < myNumber) {
+    alert('Too low!keep guessing.');
+    userNumber = prompt('What number am i Thinking of?');
+
+
+  }
+  else if (userNumber == myNumber) {
+    alert('You Guessed it right!');
+    counter = counter + 1;
+    break;
+  }
+  if (!userTry) {
+    alert('you have tried all 4 attempts');
+    break;
+  }
+}
+
+alert(`Thank you  ${userName} you got ${counter} numbers of questions correct`);
+
+
+
+
 
 
 
